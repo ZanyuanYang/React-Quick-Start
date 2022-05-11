f6e591d70bdc4db951146f62be9b3cd8c74229e2
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2022-05-10 19:07:17 -0700

    react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home, Error } from './pages';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ExampleContext } from '../context/context';
 import styled from 'styled-components';
+import { Button, Row, Col } from 'react-bootstrap';
 
 const Home = (() => {
   const { example, setExample } = React.useContext(ExampleContext);
@@ -8,6 +9,11 @@ const Home = (() => {
     <div>
       <h1>home</h1>
       <h2>{example}</h2>
+      <Row className="mx-0">
+        <Button as={Col} variant="primary">Button #1</Button>
+        <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
+        <Button as={Col} variant="success">Button #3</Button>
+      </Row>
     </div>
 
   )
