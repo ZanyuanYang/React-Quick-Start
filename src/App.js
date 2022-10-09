7303469e0fa9baa7298bf5bf47412ce10eb23454
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2022-10-08 23:20:41 -0700

    Update react v5 to v6

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+	Routes,
+	Route,
+} from "react-router-dom";
 import { Home, Error } from './pages';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path='*'>
-          <Error />
-        </Route>
-      </Switch>
-    </Router>
+    <Routes>
+		<Route exact path="/" element={<Home />}></Route>
+		<Route path="*" element={<Error />}></Route>
+	</Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ExampleProvider } from './context/context';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <ExampleProvider>
       <App />
     </ExampleProvider>
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
